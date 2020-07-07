7eb71199352ffffa719646270235ec1e71f174b2
casker2209 <casker2209@gmail.com>
2020-07-07 11:10:55 +0700

    Update VectorUnionTest.java

--- TranMinhVuong/jUnit/VectorUnionTest.java
+++ TranMinhVuong/jUnit/VectorUnionTest.java
@@ -6,53 +6,65 @@
 package softwaretestinghomework;
 
 import java.util.Vector;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author oOo
  */
 public class VectorUnionTest {
-    
+    private Vector A = new Vector();
+    private Vector B = new Vector();
+    private Vector c = new Vector();
     public VectorUnionTest() {
     
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-        
+    @Before
+    public void setUpClass() {
+        A = new Vector();
+        B = new Vector();
+        c = new Vector();for(int i = 0;i<=10;i++) A.addElement(i);
+        for(int i = -10;i<=0;i++) B.addElement(i);
+        Vector c = VectorUnion.UnionAll(A, B);
+         
     }
     
-    @AfterClass
-    public static void tearDownClass() {
+    @After
+    public void tearDownClass() {
+      A = null;
+      B = null;
+      c = null;
+    }
+    
+    @Test
+    public void testTrue(){
+            Vector c = VectorUnion.UnionAll(A, B);
+            assertTrue(c.contains(1));
+        
+    }
+    @Test
+    public void testlarge(){
+        Vector c = VectorUnion.UnionAll(A, B);        
+        assertFalse(c.contains(100000));
+    }
+    @Test
+    public void testsmall(){
+        Vector c = VectorUnion.UnionAll(A, B);
+        assertFalse(c.contains(-100000));
+    }
+    @Test
+    public void testtext(){
+        Vector c = VectorUnion.UnionAll(A, B);
+        assertFalse(c.contains("abc"));
     }
 
-    
-    /**
-     * Test of UnionAll method, of class VectorUnion.
-     */
-    @Test
-    public void testUnionAll() {
-        System.out.println("UnionAll");
-        
-        Vector A = new Vector();
-        Vector B = new Vector();
-        for(int i = 0;i<=10;i++) A.addElement(i);
-        for(int i = -10;i<=0;i++) B.addElement(i);
-        Vector c = VectorUnion.UnionAll(A,B);
-        
-        for(int i = -10;i<=10;i++){
-            assertTrue("Cool",c.contains(i));
-        }
-        assertFalse("Cool",c.contains(-100000));
-        assertFalse(c.contains(10000));
-        assertTrue(c.contains(10));
-        assertTrue(c.contains(-10));
-        assertFalse(c.contains("Abc"));
-        // TODO review the generated test code and remove the default call to fail.
-    }
+      
+   
     
 }
