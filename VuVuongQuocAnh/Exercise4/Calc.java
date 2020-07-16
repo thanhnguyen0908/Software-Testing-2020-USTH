d9edd67b54d6e7f77729cdbb35a7d7871389be58
vvquocanh <quanhuk0903@gmail.com>
2020-07-16 21:27:04 +0700

    All test fail

---
+++ VuVuongQuocAnh/Exercise4/Calc.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Calc {
+    static int add(int a, int b) {
+        return 0;
+    }
+
+    static int subtract(int a, int b) {
+        return 0;
+    }
+
+    static int multiply(int a, int b) {
+        return 0;
+    }
+
+    static int divide(int a, int b) {
+       return 0;
+    }
+}
